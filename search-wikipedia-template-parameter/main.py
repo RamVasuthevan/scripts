686f69fa50bc26bc14d701f4631ab428c4f9732b
Ram Vasuthevan <36580682+RamVasuthevan@users.noreply.github.com>
2024-08-02 01:02:04 +0000

    Checkpoint

--- search-wikipedia-template-parameter/main.py
+++ search-wikipedia-template-parameter/main.py
@@ -1,17 +1,7 @@
 import requests
-import re
 
 def get_pages_using_template(template_name, limit=500):
-    """
-    Retrieves a list of pages that embed the specified template.
-
-    Args:
-    - template_name: The name of the template to search for.
-    - limit: The maximum number of results per request.
-
-    Returns:
-    - A list of page titles using the specified template.
-    """
+    """Fetch all pages that use a given Wikipedia template."""
     url = "https://en.wikipedia.org/w/api.php"
     params = {
         "action": "query",
@@ -21,100 +11,71 @@ def get_pages_using_template(template_name, limit=500):
         "eilimit": limit,
         "continue": "",
     }
-
+    
     pages = []
     while True:
         response = requests.get(url, params=params)
         data = response.json()
-
+        
+        # Add page titles to the list
         pages.extend([item['title'] for item in data['query']['embeddedin']])
-
+        
+        # Check for continuation
         if "continue" in data:
             params.update(data["continue"])
         else:
             break
-
+    
     return pages
 
-def fetch_page_content(page_title):
-    """
-    Fetches the raw content of a Wikipedia page.
-
-    Args:
-    - page_title: The title of the Wikipedia page.
-
-    Returns:
-    - The raw content of the page.
-    """
+def page_contains_parameter(page_title, parameter_name):
+    """Check if a given page contains a specific parameter in the template."""
     url = "https://en.wikipedia.org/w/api.php"
     params = {
         "action": "query",
         "format": "json",
         "prop": "revisions",
         "titles": page_title,
-        "rvprop": "content",
         "rvslots": "main",
+        "rvprop": "content",
     }
-
+    
     response = requests.get(url, params=params)
     data = response.json()
     
-    page_id = next(iter(data['query']['pages']))
-    content = data['query']['pages'][page_id]['revisions'][0]['slots']['main']['*']
-    
-    return content
+    # Extract page content
+    page = next(iter(data['query']['pages'].values()))
+    content = page.get('revisions', [{}])[0].get('*', '')
 
-def filter_pages_by_parameter(pages, parameter):
-    """
-    Filters pages that contain the specified template parameter with a non-empty value.
+    # Check if the parameter is present in the template
+    return f"| {parameter_name} =" in content
 
-    Args:
-    - pages: A list of page titles to check.
-    - parameter: The template parameter to search for.
-
-    Returns:
-    - A list of page titles that use the specified parameter with a non-empty value.
-    """
+def filter_pages_without_parameter(pages, parameter_name):
+    """Filter out pages that contain a specific parameter."""
     filtered_pages = []
-    
-    # Regex to find non-empty parameter values
-    parameter_regex = re.compile(
-        r'{{Infobox political division[^}]*\|\s*' + re.escape(parameter) + r'\s*=\s*([^|\n}]+)', 
-        re.IGNORECASE
-    )
-
     for page in pages:
-        content = fetch_page_content(page)
-        match = parameter_regex.search(content)
-        
-        # Check if the parameter exists and has a non-empty value
-        if match and match.group(1).strip():
+        if not page_contains_parameter(page, parameter_name):
             filtered_pages.append(page)
-
     return filtered_pages
 
-def write_links_to_file(pages, filename="results.txt"):
-    """
-    Writes the Wikipedia page URLs to a text file.
-
-    Args:
-    - pages: A list of page titles.
-    - filename: The name of the output text file.
-    """
+def write_links_to_file(pages, filename="filtered_pages.txt"):
+    """Write the filtered pages' links to a text file."""
     with open(filename, "w", encoding="utf-8") as file:
         for page in pages:
+            # Format the page title into a URL
             url = f"https://en.wikipedia.org/wiki/{page.replace(' ', '_')}"
+            # Write the URL to the file
             file.write(url + "\n")
 
 if __name__ == "__main__":
     template_name = "Infobox political division"
-    parameter_to_filter = "flag_link"
-
-    # Get all pages using the specified template
+    parameter_name = "flag_link"
+    
+    # Get all pages using the template
     pages = get_pages_using_template(template_name)
-
-    # Filter pages by the specified parameter
-    filtered_pages = filter_pages_by_parameter(pages, parameter_to_filter)
-
-    # Write filtered pages to a text file
+    
+    # Filter pages that don't use the specified parameter
+    filtered_pages = filter_pages_without_parameter(pages, parameter_name)
+    
+    # Write the filtered list of pages to a text file
     write_links_to_file(filtered_pages)
