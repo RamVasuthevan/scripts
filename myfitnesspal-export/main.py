d32b3c8a803636232ccd14f8df31839d0a793945
Ram Vasuthevan <36580682+RamVasuthevan@users.noreply.github.com>
2024-09-10 18:23:33 -0400

    Refactor myfitnesspal-export (#18)
    * print each untracked file onto a new line
    
    * Add validate_environment_variables
    
    * reactor
    
    * run black

--- myfitnesspal-export/main.py
+++ myfitnesspal-export/main.py
@@ -26,20 +26,19 @@ DOGSHEEP_REPO_URL: str = "https://github.com/RamVasuthevan/dogsheep-data.git"
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(filename)s (%(lineno)d) - %(levelname)s - %(message)s",
-    handlers=[
-        logging.FileHandler(LOG_FILE)
-    ]
+    handlers=[logging.FileHandler(LOG_FILE)],
 )
 
 logger = logging.getLogger(__name__)
+
 
 def connect_to_email() -> imaplib.IMAP4_SSL:
     """Connect to the email server and log in."""
     load_dotenv()
 
-    email_user: str = os.getenv('EMAIL_USER')
-    email_password: str = os.getenv('EMAIL_PASSWORD')
-    imap_url: str = os.getenv('IMAP_URL')
+    email_user: str = os.getenv("EMAIL_USER")
+    email_password: str = os.getenv("EMAIL_PASSWORD")
+    imap_url: str = os.getenv("IMAP_URL")
 
     logger.info("Connecting to the email server")
     mail = imaplib.IMAP4_SSL(imap_url, IMAP_PORT)
@@ -47,10 +46,15 @@ def connect_to_email() -> imaplib.IMAP4_SSL:
     logger.info("Connected and logged in to the email server")
     return mail
 
-def search_and_fetch_emails(mail: imaplib.IMAP4_SSL, from_address: str, subject: str) -> List[Tuple[str, str, Message]]:
+
+def search_and_fetch_emails(
+    mail: imaplib.IMAP4_SSL, from_address: str, subject: str
+) -> List[Tuple[str, str, Message]]:
     """Search for emails from a specific sender with a specific subject and process them."""
-    logger.info(f"Searching and fetching emails from '{from_address}' with subject '{subject}'")
-    mail.select('inbox', readonly=True)
+    logger.info(
+        f"Searching and fetching emails from '{from_address}' with subject '{subject}'"
+    )
+    mail.select("inbox", readonly=True)
     search_criteria = f'(FROM "{from_address}" SUBJECT "{subject}")'
     status, messages = mail.search(None, search_criteria)
     email_ids = messages[0].split()
@@ -58,12 +62,12 @@ def search_and_fetch_emails(mail: imaplib.IMAP4_SSL, from_address: str, subject:
 
     emails = []
     for mail_id in email_ids:
-        status, msg_data = mail.fetch(mail_id, '(RFC822)')
+        status, msg_data = mail.fetch(mail_id, "(RFC822)")
         for response_part in msg_data:
             if isinstance(response_part, tuple):
                 msg = email.message_from_bytes(response_part[1])
-                message_id: str = msg.get('Message-ID')
-                date: str = msg.get('Date')
+                message_id: str = msg.get("Message-ID")
+                date: str = msg.get("Date")
 
                 if msg.is_multipart():
                     logger.info(f"{message_id} - {date}: Processing multipart email")
@@ -77,41 +81,46 @@ def search_and_fetch_emails(mail: imaplib.IMAP4_SSL, from_address: str, subject:
 
     return emails
 
+
 def process_multipart_email(msg: Message) -> Optional[str]:
     """Process a multipart email and extract the download link."""
     for part in msg.walk():
-        if part.get_content_type() == 'text/html':
+        if part.get_content_type() == "text/html":
             html_content: str = part.get_payload(decode=True).decode()
             return extract_and_return_link(html_content)
     return None
 
+
 def process_singlepart_email(msg: Message) -> Optional[str]:
     """Process a single-part email and extract the download link."""
-    if msg.get_content_type() == 'text/html':
+    if msg.get_content_type() == "text/html":
         html_content: str = msg.get_payload(decode=True).decode()
         return extract_and_return_link(html_content)
     return None
 
+
 def extract_and_return_link(html_content: str) -> Optional[str]:
     """Extract and return the download link from the HTML content."""
-    soup = BeautifulSoup(html_content, 'lxml')
-    body_div = soup.find('div', class_='mfp-default--body')
+    soup = BeautifulSoup(html_content, "lxml")
+    body_div = soup.find("div", class_="mfp-default--body")
     if body_div:
-        download_link = body_div.find('a', string='Download Files')
+        download_link = body_div.find("a", string="Download Files")
         if download_link:
-            return download_link['href']
+            return download_link["href"]
     return None
+
 
 def get_filename_from_content_disposition(headers) -> Optional[str]:
     """Extract the filename from the Content-Disposition header."""
-    content_disposition = headers.get('Content-Disposition')
+    content_disposition = headers.get("Content-Disposition")
     if content_disposition:
-        parts = content_disposition.split(';')
+        parts = content_disposition.split(";")
         for part in parts:
-            if 'filename=' in part:
-                filename = part.split('=')[1].strip('"')
+            if "filename=" in part:
+                filename = part.split("=")[1].strip('"')
                 return filename
     return None
+
 
 def get_filename_from_url(url: str) -> str:
     """Extract the filename from the URL."""
@@ -119,18 +128,21 @@ def get_filename_from_url(url: str) -> str:
     filename = os.path.basename(parsed_url.path)
     return unquote(filename)  # Decodes URL-encoded characters
 
+
 def download_file(url: str, save_dir: str) -> Optional[str]:
     """Download a file from the given URL and return the path where it was saved."""
     logger.info(f"Starting download from {url}")
     response = requests.get(url, stream=True)
-    
+
     if response.status_code == 403 and "Request has expired" in response.text:
         logger.info(f"Download link has expired: {url}")
         return None
 
     if response.status_code == 200:
         # Attempt to get the filename from the Content-Disposition header
-        filename: Optional[str] = get_filename_from_content_disposition(response.headers)
+        filename: Optional[str] = get_filename_from_content_disposition(
+            response.headers
+        )
         if not filename:
             # Fallback to extracting the filename from the URL if Content-Disposition is not available
             filename = get_filename_from_url(url)
@@ -142,7 +154,7 @@ def download_file(url: str, save_dir: str) -> Optional[str]:
         save_path: str = os.path.join(save_dir, filename)
         logger.info(f"Saving file as {save_path}")
 
-        with open(save_path, 'wb') as file:
+        with open(save_path, "wb") as file:
             for chunk in response.iter_content(chunk_size=1024):
                 if chunk:
                     file.write(chunk)
@@ -150,8 +162,11 @@ def download_file(url: str, save_dir: str) -> Optional[str]:
 
         return save_path
     else:
-        logger.error(f"Failed to download file from {url}. Status code: {response.status_code}")
+        logger.error(
+            f"Failed to download file from {url}. Status code: {response.status_code}"
+        )
         return None
+
 
 def get_script_info() -> dict:
     """
@@ -160,16 +175,16 @@ def get_script_info() -> dict:
     """
     try:
         repo = Repo(os.path.dirname(__file__), search_parent_directories=True)
-        
+
         # Get the current commit hash
         commit_hash = repo.head.commit.hexsha
-        
+
         # Check if there are uncommitted changes
         uncommitted_changes = repo.is_dirty(untracked_files=True)
-        
+
         # Get the remote origin URL (i.e., the repository URL)
         repo_url = next(repo.remote().urls)
-        
+
         return {
             "repository": repo_url,
             "commit_hash": commit_hash,
@@ -183,6 +198,7 @@ def get_script_info() -> dict:
             "uncommitted_changes": None,
         }
 
+
 def clone_dogsheep_data(branch: str) -> str:
     """Clone the dogsheep-data repository to a temporary directory."""
     repo_dir = "dogsheep-data"
@@ -192,13 +208,16 @@ def clone_dogsheep_data(branch: str) -> str:
     Repo.clone_from(DOGSHEEP_REPO_URL, repo_dir, branch=branch)
     return repo_dir
 
-def write_extracted_files(zip_path: str, extract_dir: str, message_id: str, email_message: Message):
+
+def write_extracted_files(
+    zip_path: str, extract_dir: str, message_id: str, email_message: Message
+):
     """Extract the ZIP file, write metadata about the email, and delete the ZIP file."""
     # Ensure the extract directory exists
     os.makedirs(extract_dir, exist_ok=True)
 
     # Extract the contents of the ZIP file
-    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+    with zipfile.ZipFile(zip_path, "r") as zip_ref:
         zip_ref.extractall(extract_dir)
         logger.info(f"Extracted contents of {zip_path} to {extract_dir}")
 
@@ -216,7 +235,7 @@ def write_extracted_files(zip_path: str, extract_dir: str, message_id: str, emai
         "uncommitted_changes": git_info.get("uncommitted_changes"),
     }
     metadata_path = os.path.join(extract_dir, "metadata.json")
-    with open(metadata_path, 'w') as metadata_file:
+    with open(metadata_path, "w") as metadata_file:
         json.dump(metadata, metadata_file, indent=4)
     logger.info(f"Wrote metadata to {metadata_path}")
 
@@ -224,17 +243,13 @@ def write_extracted_files(zip_path: str, extract_dir: str, message_id: str, emai
     os.remove(zip_path)
     logger.info(f"Deleted the ZIP file: {zip_path}")
 
+
 def extract_date_range_from_filename(zip_path: str) -> str:
     """Extract the date range from the ZIP filename."""
     filename = os.path.basename(zip_path)
     date_range = filename.split("File-Export-")[-1].split(".zip")[0]
     return date_range
 
-import os
-from git import Repo, InvalidGitRepositoryError
-import logging
-
-logger = logging.getLogger(__name__)
 
 def commit_untracked_files_to_repo(repo_dir: str, script_name: str):
     """Commit only untracked files in the dogsheep-data repository."""
@@ -243,7 +258,9 @@ def commit_untracked_files_to_repo(repo_dir: str, script_name: str):
 
         # Get a list of all untracked files in the extract_dir
         untracked_files = repo.untracked_files
-        print(f"Untracked files: {untracked_files}")
+        print(f"Untracked files:")
+        for file in untracked_files:
+            print("\t", file)
 
         if untracked_files:
             # Stage only untracked files
@@ -262,7 +279,7 @@ def commit_untracked_files_to_repo(repo_dir: str, script_name: str):
             # Commit the changes
             repo.index.commit(commit_message)
             # Push the changes
-            origin = repo.remote(name='origin')
+            origin = repo.remote(name="origin")
             origin.push()
             logger.info(f"Committed and pushed untracked files for MyFitnessPal Export")
         else:
@@ -273,16 +290,14 @@ def commit_untracked_files_to_repo(repo_dir: str, script_name: str):
         logger.error(f"Failed to commit untracked files: {e}")
 
 
-
 def format_date_for_folder(date_str: str) -> str:
     """Format the email date string for use in a folder name."""
     date_obj = email.utils.parsedate_to_datetime(date_str)
-    return date_obj.strftime('%Y%m%d_%H%M%S')
+    return date_obj.strftime("%Y%m%d_%H%M%S")
 
-def process_emails(mail: imaplib.IMAP4_SSL, branch: str):
+
+def process_emails(emails: List[Tuple[str, str, Message]]):
     """Process all relevant emails."""
-    repo_dir = clone_dogsheep_data(branch)
-    emails = search_and_fetch_emails(mail, FROM_ADDRESS, SUBJECT)
     for message_id, download_link, email_message in emails:
         logger.info(f"{message_id}: Download link: {download_link}")
         # Format the date for the folder name
@@ -291,24 +306,47 @@ def process_emails(mail: imaplib.IMAP4_SSL, branch: str):
         zip_path = download_file(download_link, save_dir=SAVE_DIR)
         if zip_path:
             # Set the extraction directory and write metadata
-            extract_dir = os.path.join(SAVE_DIR, f"{formatted_date}_{os.path.splitext(os.path.basename(zip_path))[0]}_{message_id}")
-
+            extract_dir = os.path.join(
+                SAVE_DIR,
+                f"{formatted_date}_{os.path.splitext(os.path.basename(zip_path))[0]}_{message_id}",
+            )
             write_extracted_files(zip_path, extract_dir, message_id, email_message)
         else:
-            logger.warning(f"{message_id}: Skipping processing due to expired download link.")
-        
-    commit_untracked_files_to_repo(repo_dir, os.path.basename(__file__))
+            logger.warning(
+                f"{message_id}: Skipping processing due to expired download link."
+            )
+
+
+def validate_environment_variables():
+    """Validate that all required environment variables are set."""
+    required_vars = ["EMAIL_USER", "EMAIL_PASSWORD", "IMAP_URL"]
+    for var in required_vars:
+        if not os.getenv(var):
+            raise ValueError(f"Environment variable {var} is not set")
+
 
 def main():
     logger.info("Starting the email processing script")
-    branch = os.getenv("DOGSHEEP_BRANCH", "test-myfitnesspal-export")
-    mail = connect_to_email()
-    
+    validate_environment_variables()
+    branch = "main"
+
     try:
-        process_emails(mail, branch)
+        # Clone the dogsheep-data repository
+        repo_dir = clone_dogsheep_data(branch)
+
+        # Connect to email
+        mail = connect_to_email()
+        emails = search_and_fetch_emails(mail, FROM_ADDRESS, SUBJECT)
+        # Process the emails
+        process_emails(emails)
+
+        # Commit untracked files to the repository
+        commit_untracked_files_to_repo(repo_dir, os.path.basename(__file__))
+
     finally:
         logger.info("Logging out from the email server")
         mail.logout()
+
 
 if __name__ == "__main__":
     main()
